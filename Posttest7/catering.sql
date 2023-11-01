e701e3950b837f84cc40a9c896ec95f8a943c225
Andi Zahrina Athirah Ahmad <117283292+azahrinath@users.noreply.github.com>
2023-11-02 00:07:28 +0800

    Add files via upload

--- Posttest7/catering.sql
+++ Posttest7/catering.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 25 Okt 2023 pada 17.41
+-- Waktu pembuatan: 01 Nov 2023 pada 17.06
 -- Versi server: 10.4.28-MariaDB
 -- Versi PHP: 8.2.4
 
@@ -47,6 +47,27 @@ INSERT INTO `pesanan` (`id`, `name`, `datetime`, `quantity`, `address`, `message
 (19, 'Dinda', '2023-10-11 03:36:00', '25', 'Jl. M Yamin', 'untuk acara kegiatan kampus', '2023-10-24 Profil3.png.png'),
 (20, 'Vista', '2023-10-06 12:40:00', '30', 'Jl. Pramuka', 'untuk acara party', '6539365a4aa45_2023-10-25 Profil4.png');
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(13, 'zahrina', 'azahrinathiraha051@gmail.com', '$2y$10$YCsQP5R3Ct5zuqczlOP8GOl8ySwEGGDLGUsdi.WuM.rJi3LG6MGFu'),
+(14, 'arin', 'arin@gmail.com', '$2y$10$eppJxNlU00eh88Gaf3liJOAUubZZHGC4ycg7admDpXvRz0Gk.y63e');
+
 --
 -- Indexes for dumped tables
 --
@@ -58,6 +79,12 @@ ALTER TABLE `pesanan`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -65,7 +92,13 @@ ALTER TABLE `pesanan`
 -- AUTO_INCREMENT untuk tabel `pesanan`
 --
 ALTER TABLE `pesanan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
